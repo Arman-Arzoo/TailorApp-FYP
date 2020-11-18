8299645f371c029ab7ef829ff32570c22cc54beb
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-11-18 23:03:40 +0500

    whychoose.js is added with image and  it component and css

--- tailor-frontend/src/App.js
+++ tailor-frontend/src/App.js
@@ -4,6 +4,8 @@ import {Header} from './components/Header';
 import {Footer} from './components/Footer'
 import {ImageSliders} from './components/Home/ImageSliders';
 import {HowItWork} from './components/Home/HowItWork';
+import {WhyChoose} from './components/Home/WhyChoose';
+import {OurTeam} from './components/Home/OurTeam';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <main className="main">
         <ImageSliders/>
         <HowItWork/>
+        <WhyChoose/>
+        <OurTeam/>
       
       </main>
       <Footer name="Arman ALi"/>
---
+++ tailor-frontend/src/components/Home/OurTeam.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const OurTeam = () => {
+    return (
+        <div className="our-team">
+        <h3 className="text-center">Our Team</h3>
+        <div className="team">
+            <div>
+                <img src="arman.PNG" alt="ibrahim" ></img>   
+            </div>
+            <div>
+                <img src="arman.PNG" alt="arman"></img>
+                <h3>Arman Ali</h3>
+            </div>
+            <div>
+                <img src="arman.PNG" alt="shoaib" ></img>
+                
+            </div>
+
+
+        </div>
+
+    </div>
+    )
+}
