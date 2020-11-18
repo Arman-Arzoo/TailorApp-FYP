63213fede2bcd46782c2688e7dd55cbc5be8c8c3
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-11-18 17:52:07 +0500

    Media query for header
    Mobile response header

--- tailor-frontend/src/components/Footer.js
+++ tailor-frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = ({name}) => {
     return (
         <div className="footer">
-             CopyRight @ 2020. All right reserved. Created by {name}
+             <p>CopyRight @ 2020. All right reserved. Created by {name}</p>
         </div>
     )
 }
