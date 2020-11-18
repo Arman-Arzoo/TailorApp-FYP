e7efa06becd8c72efc71880266952b411199cb5c
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-11-18 22:24:20 +0500

    Make a slider and how it work component and add some css
    In this commit we add images and make two component ImageSliders.js and HowItwork.js and make two folder in images one is slider and other is howItWork and add css and media query for small mobile

--- tailor-frontend/src/App.js
+++ tailor-frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter} from "react-router-dom";
 import {Header} from './components/Header';
 import {Footer} from './components/Footer'
+import {ImageSliders} from './components/Home/ImageSliders';
+import {HowItWork} from './components/Home/HowItWork';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
      
       <Header/>
       <main className="main">
+        <ImageSliders/>
+        <HowItWork/>
       
       </main>
       <Footer name="Arman ALi"/>
