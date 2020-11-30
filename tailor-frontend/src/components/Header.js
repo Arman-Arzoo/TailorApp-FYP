ad34d19fcef47d6b71d44fdebe5c34068ce7e52e
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-11-30 21:50:35 +0500

    update app.css and Header.js when user login
    add bell notifcation and profile name with pic in header.js

--- tailor-frontend/src/components/Header.js
+++ tailor-frontend/src/components/Header.js
@@ -1,14 +1,14 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import {GlobalContext} from './../context/GlobalContexts';
+import { GlobalContext } from "./../context/GlobalContexts";
+import {AiFillBell} from 'react-icons/ai'
 
 export const Header = () => {
+  const { userData } = useContext(GlobalContext);
 
-  const {userData} = useContext(GlobalContext);
-
-  const firstname =((userData || {}).user || {}).firstName;
-  const lastname =((userData || {}).user || {}).lastName;
-  const userName = firstname +" "+lastname;
+  const firstname = ((userData || {}).user || {}).firstName;
+  const lastname = ((userData || {}).user || {}).lastName;
+  const userName = firstname + " " + lastname;
 
   return (
     <div>
@@ -16,16 +16,28 @@ export const Header = () => {
         <div className="header-logo">
           <Link to="/">Tailor App</Link>
         </div>
+       
         <div className="header-menu">
+          {userData.user ? (
+            <>
+            <dev className="users-notification">
+                <AiFillBell/>
+              </dev>
 
-          {userData.user ? (<> 
-
-            <Link to="/udashboard" >{userName }</Link>
-          </>):(<> 
-            <Link to="/signup">Sign Up</Link>
-            <Link to="/signin">Sign In</Link>
-          </>)}
-          
+              <Link to="/udashboard"> 
+                <div className="header-profile">
+                  <img src="/images/OurTeam/arman.PNG" alt="arman"></img>
+                  <span className="header-name">{userName} </span>
+                </div>
+              </Link>
+              
+            </>
+          ) : (
+            <>
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/signin">Sign In</Link>
+            </>
+          )}
         </div>
       </div>
     </div>
